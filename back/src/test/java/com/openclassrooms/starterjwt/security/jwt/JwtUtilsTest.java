748221e5190ade51a52e31f21e2e8c109cdf4d62
Ayvenou <axel.yvenou@gmail.com>
2024-04-01 06:20:41 +0200

    jwt utils tests

--- back/src/test/java/com/openclassrooms/starterjwt/security/jwt/JwtUtilsTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/jwt/JwtUtilsTest.java
@@ -1,15 +1,19 @@
 package com.openclassrooms.starterjwt.security.jwt;
 
 import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -19,34 +23,78 @@ class JwtUtilsTest {
     @Autowired
     private JwtUtils jwtUtils;
 
+    @Value("${oc.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${oc.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
     @Test
     void testGenerateJwtToken() {
-        // Mock the SecurityContextHolder
         Authentication authentication = Mockito.mock(Authentication.class);
         UserDetailsImpl userDetails = Mockito.mock(UserDetailsImpl.class);
         when(authentication.getPrincipal()).thenReturn(userDetails);
         when(userDetails.getUsername()).thenReturn("test");
 
-        // Test the method
         String token = jwtUtils.generateJwtToken(authentication);
 
-        // Assert
         assertEquals("test", jwtUtils.getUserNameFromJwtToken(token));
     }
 
     @Test
     void testValidateJwtToken() {
-        // Mock the SecurityContextHolder
         Authentication authentication = Mockito.mock(Authentication.class);
         UserDetailsImpl userDetails = Mockito.mock(UserDetailsImpl.class);
         when(authentication.getPrincipal()).thenReturn(userDetails);
         when(userDetails.getUsername()).thenReturn("test");
 
-        // Test the method
         String token = jwtUtils.generateJwtToken(authentication);
 
-        // Assert
         assertTrue(jwtUtils.validateJwtToken(token));
+    }
+
+    @Test
+    void testValidateJWTTokenWithInvalidJToken() {
+        boolean result = jwtUtils.validateJwtToken("invalidToken");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testValidateJWTTokenWithExpiredToken() {
+        String token = Jwts.builder().setSubject("test")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() - jwtExpirationMs - 1))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+
+        assertFalse(jwtUtils.validateJwtToken(token));
+    }
+
+    @Test
+    void testValidateJWTTokenWithInvalidSignature() {
+        String token = Jwts.builder().setSubject("test")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, "invalidSecret")
+                .compact();
+
+        assertFalse(jwtUtils.validateJwtToken(token));
+    }
+
+    @Test
+    void testValidateJWTTokenWithUnsupportedToken() {
+        String token = Jwts.builder().setSubject("test")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .compact();
+
+        assertFalse(jwtUtils.validateJwtToken(token));
+    }
+
+    @Test
+    void testValidateJWTTokenWithInvalidClaim() {
+        assertFalse(jwtUtils.validateJwtToken(""));
     }
 
 }
